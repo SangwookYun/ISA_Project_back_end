c4770e288cfde3ad6a4e1c0c053f3dc1894d4a15
sangwookYun <brtm0533@naver.com>
2020-11-20 14:24:17 -0800

    change writeHead

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express()
 const PORT = process.env.PORT ||3000
 
 
-app.get('', (req, res)=> {
+app.get('/', (req, res)=> {
+    res.writeHead(200, { "Content-Type": "text/html", "Access-Control-Allow-Origin": "*" });
     res.send("Hello Jason!")
 })
 
